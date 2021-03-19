447ffdc00d87ad3742250c75fec8aea9036a43e2
hemel18681 <hemel18103112@gmail.com>
2021-03-20 02:08:54 +0600

    report and post sell done button also report about post remaining

--- main_app/main_app/urls.py
+++ main_app/main_app/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path('login/',include('authenticate.urls')),
     path('post_info/',include('post_details.urls')),
     path('admin_page/', views.admin_page, name='admin_page'),
+    path('post/<int:post_id>/',views.post_view,name='post_details'),
+    path('post_image/<int:post_id>/',views.indivisual_view_pic,name='indivisual_view'),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- main_app/main_app/views.py
+++ main_app/main_app/views.py
@@ -13,6 +13,29 @@ def home(request):
         'all_post':all_post,
     }
     return render(request,'index.html',context)
+    
 
 def admin_page(request):
     return redirect('/admin/')
+
+def post_view(request,post_id):
+    id = post_id
+    post_details = running_post.objects.filter(id=id)
+    details = post_details.values()
+    print(details)
+    user_phone_number = details[0]['user_phone_number']
+    user_details = user_info.objects.filter(user_phone_number = user_phone_number)
+    context = {
+        'post_details':post_details,
+        'user_details':user_details,
+    }
+    return render(request,'post_preview.html',context)
+
+def indivisual_view_pic(request,post_id):
+    id = post_id
+    post_details = running_post.objects.filter(id=id)
+    details = post_details.values()
+    context = {
+        'post_details':post_details,
+    }
+    return render(request,'full_picture.html',context)
